e1b73ca0ea90d7f91d658d580eba49f51c726598
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2022-02-06 17:22:42 +0200

    refactor: mv App component to components directory

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="center">Top Starred Repos in the last 30 days</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
